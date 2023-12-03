6d0fecb032f6f57d963fd12e1e01e73606d42b84
artёm rudoy <art.rudy@gmail.com>
2023-12-03 23:21:48 +0200

    code update (oder)

--- app.ts
+++ app.ts
@@ -1,12 +1,58 @@
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+
+const ctx = canvas?.getContext("2d") as CanvasRenderingContext2D;
+
 const gridColumns = 10;
+
 const gridRows = 20;
 
-const vacant = "black";
+const SQ = canvas.height / gridRows;
+
+console.log(SQ);
+
+const vacant = "white";
+
 const color = "darkgreen";
+
+function drawSquare(x, y, color) {
+  ctx.fillStyle = `${color}`;
+  ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
+}
+
+drawSquare(1, 1, color);
 
 let board: string[][] = [];
 
+for (let r = 0; r < gridRows; r += 1) {
+  board[r] = [];
+  for (let c = 0; c < gridColumns; c += 1) {
+    board[r][c] = vacant;
+  }
+}
 
+function drawBoard() {
+  for (let r = 0; r < gridRows; r += 1) {
+    for (let c = 0; c < gridColumns; c += 1) {
+      drawSquare(c, r, board[r][c]);
+    }
+  }
+}
+drawBoard();
+
+function Piece(tetromino, color) {
+  this.tetromino = tetromino;
+  this.color = color;
+
+  this.tetrominoN = 0;
+  this.activeTetromino = this.tetromino[this.tetrominoN];
+
+  this.x = 2;
+  this.y = 4;
+}
+
+drawSquare(0, 0, color);
 
 const Z = [
   [
@@ -177,17 +223,19 @@ const O = [
   ],
 ];
 
+const PIECES = [
+  [Z, "red"],
+  [S, "darkgreen"],
+  [T, "darkgreen"],
+  [O, "darkgreen"],
+  [L, "darkgreen"],
+  [I, "darkgreen"],
+  [J, "darkgreen"],
+];
+
 const Pieces = [Z, S, T, O, I, L, J];
 
-
-
-function Piece(Tetromino) {
-  this.tetromino = tetromino;
-  this.tetrominoN = 0;
-  this.activeTetromino = this.tetromino[this.tetrominoN];
-  this.x = 3;
-  this.y = -2;
-}
+let p = new Piece(PIECES[0][0], PIECES[0][1]);
 
 for (let r = 0; r <= gridRows; r += 1) {
   board[r] = [];
@@ -196,229 +244,170 @@ for (let r = 0; r <= gridRows; r += 1) {
   }
 }
 
-const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-const ctx = canvas?.getContext("2d") as CanvasRenderingContext2D;
-
-const SQ = canvas.height / gridRows;
-console.log(SQ);
-
-function drawSquare(x, y, color) {
-  ctx.fillStyle = `${color}`;
-  ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
-}
-
-function drawBoard() {
-  for (let r = 0; r < gridRows; r += 1) {
-    for (let c = 0; c < gridColumns; c += 1) drawSquare(c, r, board[r][c]);
-  }
-}
-
-drawBoard();
-
-drawSquare(0, 0, color);
-
-// function drawTetromino() {
-//   let piece = J[0];
-//   for (let r = 0; r < piece.length; r += 1) {
-//     for (let c = 0; c < piece.length; c += 1) {
-//       if (piece[r][c]) {
-//         drawSquare(c, r, color);
-//       }
-//     }
-//   }
-// }
-
-// drawTetromino();
-
-function draw() {
-  Piece.prototype.draw = function () {
-    for (let r = 0; r < this.activeTetromino.length; r += 1){
-      for (let c = 0; c < this.activeTetromino[r][c]){
+Piece.prototype.fill = function (color: string) {
+  for (let r = 0; r < this.activeTetromino.length; r += 1) {
+    for (let c = 0; c < this.activeTetromino.length; c += 1) {
+      if (this.activeTetromino[r][c]) {
         drawSquare(this.x + c, this.y + r, color);
       }
     }
   }
-}
+};
 
-function unDraw() {
-  Piece.prototype.draw = function () {
-    for (let r = 0; r < this.activeTetromino.length; r += 1){
-      for (let c = 0; c < this.activeTetromino[r][c]){
-        drawSquare(this.x + c, this.y + r, 'black');
-      }
-    }
-  }
-}
+Piece.prototype.draw = function () {
+  this.fill(this.color);
+};
 
-
-document.addEventListener('keydown', control);
-
-function control(event: KeyboardEvent) {
-  const { key } = event;
-
-  if (key === "ArrowLeft") {
-    piece.moveLeft();
-    dropStart = Date.now();
-  } else if (key === "ArrowUp") {
-    piece.rotate();
-    dropStart = Date.now();
-
-  } else if (key === "ArrowRight") {
-    piece.moveRight();
-    dropStart = Date.now();
-
-  } else if (key === "ArrowDown") {
-    piece.moveDown();
-  }
-}
-
-
-Piece.prototype.collision = function (x, y, piece) {
-  
-}
+Piece.prototype.unDraw = function () {
+  // this.draw(vacant);
+  this.fill(vacant);
+};
+p.draw();
 
 Piece.prototype.moveDown = function () {
-  if (!this.collision(0, 1, this.activeTetromino)) {
+  if (!this.collision()) {
     this.unDraw();
-    this.y++;
-    this.draw();  
+    this.y += 1;
+    this.draw();
   } else {
-    
-  }  
-}
-
-Piece.prototype.moveLeft = function () {
-    if (!this.collision(-1, 0, this.activeTetromino)) {
-  this.unDraw();
-  this.x--;
-      this.draw();
-    } else {
-      this.lock();
-      piece = randomPiece();
-    
-  }  
-}
+  }
+};
 
 Piece.prototype.moveRight = function () {
-      if (!this.collision(1, 0, this.activeTetromino)) {
+  if (!this.collision()) {
+    this.unDraw();
+    this.x++;
+    this.draw();
+  } else {
+  }
+};
 
-  this.unDraw();
-  this.x++;
-  this.draw();  
-} else {
-    
-  }  
-}
+Piece.prototype.moveLeft = function () {
+  if (!this.collision()) {
+    this.unDraw();
+    this.x--;
+    this.draw();
+  } else {
+  }
+};
 
+Piece.prototype.rotate = function () {
+  if (!this.collision()) {
+    this.unDraw();
+    this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    this.draw();
+  }
+};
 
 Piece.prototype.collision = function (x: number, y: number, piece: number[][]) {
-  for (let r = 0; r < piece.length; r += 1){
-    for (let c = 0; c < piece.length; c += 1){
-      if (!piece[r][c]) { continue }
-      
-      let newX = this.x + c + x;
-      
-      let newY = this.y + r + y;
-
-      if (newX < 0 || newX >= gridColumns || newY > gridRows) {
-        return true
+  for (let r = 0; r < piece.length; r += 1) {
+    for (let c = 0; c < piece.length; c += 1) {
+      if (!piece[r][c]) {
+        continue;
       }
 
-      if (newY < 0) { continue };
+      let newX = this.x + c + x;
+
+      let newY = this.y + r + y;
+
+      if (newX < 0 || newX >= gridColumns || newY >= gridRows) {
+        return true;
+      }
+
+      if (newY < 0) {
+        continue;
+      }
 
       if (board[newY][newX] !== vacant) {
         return true;
       }
     }
   }
-}
+  return false;
+};
 
-Piece.prototype.rotate = function () {
+document.addEventListener("keydown", control);
 
-  let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
+function control(event: KeyboardEvent): void {
+  const { code } = event;
 
-  let kick = 0;
-
-  if (this.collision(0, 0, nextPattern)) {
-    if (this.x > gridColumns / 2) {
-      kick = -1;
-    } else {
-      kick = 1;
-    }
-  }
-
-  if (!this.collision(kick, 0, nextPattern)) {
-    this.unDraw();
-    this.x += kick;
-    this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
-    this.activeTetromino = this.tetromino[this.tetrominoN];
-    this.draw();  
-  } 
-}
-
-function randomPiece() {
-  let randomN = Math.floor(Math.random() * Pieces.length);
-
-  return new Piece(Pieces[randomN]);
-}
-
-Piece.prototype.lock = function () {
-  for (let r = 0; r < this.activeTetromino.length; r += 1) {
-    for (let c = 0; c < this.activeTetromino.length; c += 1){
-      if (!this.activeTetromino[r][c]) {
-        continue;
-      }
-      if (this.y + r < 0) {
-        gameOver = true;
-        alert('game over!');
-        break;
-      }
-      board[this.y + r][this.x + c] = color;
-    }
+  switch (code) {
+    case "ArrowLeft":
+      p.moveLeft();
+      dropStart = Date.now();
+      break;
+    case "ArrowUp":
+      p.rotate();
+      dropStart = Date.now();
+      break;
+    case "ArrowRight":
+      p.moveRight();
+      dropStart = Date.now();
+      break;
+    case "ArrowDown":
+      p.moveDown();
+      dropStart = Date.now();
+      break;
   }
 }
-
-function fullRow(){
-  for (let r = 0; r < gridRows; r += 1){
-    let isRowFulll = true;
-    for (let c = 0; c < gridColumns; c += 1){
-      isRowFulll = isRowFulll&&(board[r][c] != vacant)
-    }
-    if (isRowFulll) {
-      for (let y = r; y > 1; y -= 1){
-        for (let c = 0; c < gridColumns; c += 1){
-          board[y][c] = board[y - 1][c];
-        }
-        board[8][10] = board[7][10]
-      }
-      for (let c = 0; c < gridColumns; c += 1){
-        board[0][c] = vacant;
-      }
-      score += 10;
-    }
-  }
-  drawBoard();
-}
-
 
 let dropStart = Date.now();
 
-let gameOver = false;
-  
-  function drop() {
-    let now = Date.now();
+function drop() {
+  let now = Date.now();
+  let delta = now - dropStart;
 
-    let delta = now - dropStart;
-
-    if (delta > 1000) {
-      piece.moveDown();
-      dropStart = Date.now();
-    }
-    if (!gameOver) {
-      requestAnimationFrame(drop);
-    }
+  if (delta > 1000) {
+    p.moveDown();
+    dropStart = Date.now();
   }
 
+  requestAnimationFrame(drop);
+}
+
 drop();
+
+// function randomPiece() {
+//   let randomN = Math.floor(Math.random() * Pieces.length);
+
+//   return new Piece(Pieces[randomN]);
+// }
+
+// Piece.prototype.lock = function () {
+//   for (let r = 0; r < this.activeTetromino.length; r += 1) {
+//     for (let c = 0; c < this.activeTetromino.length; c += 1){
+//       if (!this.activeTetromino[r][c]) {
+//         continue;
+//       }
+//       if (this.y + r < 0) {
+//         gameOver = true;
+//         alert('game over!');
+//         break;
+//       }
+//       board[this.y + r][this.x + c] = color;
+//     }
+//   }
+// }
+
+// function fullRow(){
+//   for (let r = 0; r < gridRows; r += 1){
+//     let isRowFulll = true;
+//     for (let c = 0; c < gridColumns; c += 1){
+//       isRowFulll = isRowFulll&&(board[r][c] != vacant)
+//     }
+//     if (isRowFulll) {
+//       for (let y = r; y > 1; y -= 1){
+//         for (let c = 0; c < gridColumns; c += 1){
+//           board[y][c] = board[y - 1][c];
+//         }
+//         board[8][10] = board[7][10]
+//       }
+//       for (let c = 0; c < gridColumns; c += 1){
+//         board[0][c] = vacant;
+//       }
+//       score += 10;
+//     }
+//   }
+//   drawBoard();
+// }
--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,42 @@
+var canvas = document.querySelector("#canvas");
+var ctx = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
 var gridColumns = 10;
 var gridRows = 20;
-var vacant = "black";
+var SQ = canvas.height / gridRows;
+console.log(SQ);
+var vacant = "white";
 var color = "darkgreen";
+function drawSquare(x, y, color) {
+    ctx.fillStyle = "" + color;
+    ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
+}
+drawSquare(1, 1, color);
 var board = [];
+for (var r = 0; r < gridRows; r += 1) {
+    board[r] = [];
+    for (var c = 0; c < gridColumns; c += 1) {
+        board[r][c] = vacant;
+    }
+}
+function drawBoard() {
+    for (var r = 0; r < gridRows; r += 1) {
+        for (var c = 0; c < gridColumns; c += 1) {
+            drawSquare(c, r, board[r][c]);
+        }
+    }
+}
+drawBoard();
+function Piece(tetromino, color) {
+    this.tetromino = tetromino;
+    this.color = color;
+    this.tetrominoN = 0;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    this.x = 2;
+    this.y = 4;
+}
+drawSquare(0, 0, color);
 var Z = [
     [
         [1, 1, 0],
@@ -165,115 +199,73 @@ var O = [
         [0, 0, 0, 0],
     ],
 ];
+var PIECES = [
+    [Z, "red"],
+    [S, "darkgreen"],
+    [T, "darkgreen"],
+    [O, "darkgreen"],
+    [L, "darkgreen"],
+    [I, "darkgreen"],
+    [J, "darkgreen"],
+];
 var Pieces = [Z, S, T, O, I, L, J];
-function Piece(Tetromino) {
-    this.tetromino = tetromino;
-    this.tetrominoN = 0;
-    this.activeTetromino = this.tetromino[this.tetrominoN];
-    this.x = 3;
-    this.y = -2;
-}
+var p = new Piece(PIECES[0][0], PIECES[0][1]);
 for (var r = 0; r <= gridRows; r += 1) {
     board[r] = [];
     for (var c = 0; c <= gridColumns; c += 1) {
         board[r][c] = vacant;
     }
 }
-var canvas = document.querySelector("#canvas");
-var ctx = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
-var SQ = canvas.height / gridRows;
-console.log(SQ);
-function drawSquare(x, y, color) {
-    ctx.fillStyle = "" + color;
-    ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
-}
-function drawBoard() {
-    for (var r = 0; r < gridRows; r += 1) {
-        for (var c = 0; c < gridColumns; c += 1)
-            drawSquare(c, r, board[r][c]);
-    }
-}
-drawBoard();
-drawSquare(0, 0, color);
-// function drawTetromino() {
-//   let piece = J[0];
-//   for (let r = 0; r < piece.length; r += 1) {
-//     for (let c = 0; c < piece.length; c += 1) {
-//       if (piece[r][c]) {
-//         drawSquare(c, r, color);
-//       }
-//     }
-//   }
-// }
-// drawTetromino();
-function draw() {
-    Piece.prototype.draw = function () {
-        for (var r = 0; r < this.activeTetromino.length; r += 1) {
-            for (var c = 0; c < this.activeTetromino[r][c];) {
+Piece.prototype.fill = function (color) {
+    for (var r = 0; r < this.activeTetromino.length; r += 1) {
+        for (var c = 0; c < this.activeTetromino.length; c += 1) {
+            if (this.activeTetromino[r][c]) {
                 drawSquare(this.x + c, this.y + r, color);
             }
         }
-    };
-}
-function unDraw() {
-    Piece.prototype.draw = function () {
-        for (var r = 0; r < this.activeTetromino.length; r += 1) {
-            for (var c = 0; c < this.activeTetromino[r][c];) {
-                drawSquare(this.x + c, this.y + r, 'black');
-            }
-        }
-    };
-}
-document.addEventListener('keydown', control);
-function control(event) {
-    var key = event.key;
-    if (key === "ArrowLeft") {
-        piece.moveLeft();
-        dropStart = Date.now();
     }
-    else if (key === "ArrowUp") {
-        piece.rotate();
-        dropStart = Date.now();
-    }
-    else if (key === "ArrowRight") {
-        piece.moveRight();
-        dropStart = Date.now();
-    }
-    else if (key === "ArrowDown") {
-        piece.moveDown();
-    }
-}
-Piece.prototype.collision = function (x, y, piece) {
 };
+Piece.prototype.draw = function () {
+    this.fill(this.color);
+};
+Piece.prototype.unDraw = function () {
+    // this.draw(vacant);
+    this.fill(vacant);
+};
+p.draw();
 Piece.prototype.moveDown = function () {
-    if (!this.collision(0, 1, this.activeTetromino)) {
+    if (!this.collision()) {
         this.unDraw();
-        this.y++;
+        this.y += 1;
+        this.draw();
+    }
+    else {
+    }
+};
+Piece.prototype.moveRight = function () {
+    if (!this.collision()) {
+        this.unDraw();
+        this.x++;
         this.draw();
     }
     else {
     }
 };
 Piece.prototype.moveLeft = function () {
-    if (!this.collision(-1, 0, this.activeTetromino)) {
+    if (!this.collision()) {
         this.unDraw();
         this.x--;
         this.draw();
     }
     else {
-        this.lock();
-        piece = randomPiece();
     }
 };
-Piece.prototype.moveRight = function () {
-    if (!this.collision(1, 0, this.activeTetromino)) {
+Piece.prototype.rotate = function () {
+    if (!this.collision()) {
         this.unDraw();
-        this.x++;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
+        this.activeTetromino = this.tetromino[this.tetrominoN];
         this.draw();
-    }
-    else {
     }
 };
 Piece.prototype.collision = function (x, y, piece) {
@@ -284,89 +276,89 @@ Piece.prototype.collision = function (x, y, piece) {
             }
             var newX = this.x + c + x;
             var newY = this.y + r + y;
-            if (newX < 0 || newX >= gridColumns || newY > gridRows) {
+            if (newX < 0 || newX >= gridColumns || newY >= gridRows) {
                 return true;
             }
             if (newY < 0) {
                 continue;
             }
-            ;
             if (board[newY][newX] !== vacant) {
                 return true;
             }
         }
     }
+    return false;
 };
-Piece.prototype.rotate = function () {
-    var nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
-    var kick = 0;
-    if (this.collision(0, 0, nextPattern)) {
-        if (this.x > gridColumns / 2) {
-            kick = -1;
-        }
-        else {
-            kick = 1;
-        }
+document.addEventListener("keydown", control);
+function control(event) {
+    var code = event.code;
+    switch (code) {
+        case "ArrowLeft":
+            p.moveLeft();
+            dropStart = Date.now();
+            break;
+        case "ArrowUp":
+            p.rotate();
+            dropStart = Date.now();
+            break;
+        case "ArrowRight":
+            p.moveRight();
+            dropStart = Date.now();
+            break;
+        case "ArrowDown":
+            p.moveDown();
+            dropStart = Date.now();
+            break;
     }
-    if (!this.collision(kick, 0, nextPattern)) {
-        this.unDraw();
-        this.x += kick;
-        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
-        this.activeTetromino = this.tetromino[this.tetrominoN];
-        this.draw();
-    }
-};
-function randomPiece() {
-    var randomN = Math.floor(Math.random() * Pieces.length);
-    return new Piece(Pieces[randomN]);
-}
-Piece.prototype.lock = function () {
-    for (var r = 0; r < this.activeTetromino.length; r += 1) {
-        for (var c = 0; c < this.activeTetromino.length; c += 1) {
-            if (!this.activeTetromino[r][c]) {
-                continue;
-            }
-            if (this.y + r < 0) {
-                gameOver = true;
-                alert('game over!');
-                break;
-            }
-            board[this.y + r][this.x + c] = color;
-        }
-    }
-};
-function fullRow() {
-    for (var r = 0; r < gridRows; r += 1) {
-        var isRowFulll = true;
-        for (var c = 0; c < gridColumns; c += 1) {
-            isRowFulll = isRowFulll && (board[r][c] != vacant);
-        }
-        if (isRowFulll) {
-            for (var y = r; y > 1; y -= 1) {
-                for (var c = 0; c < gridColumns; c += 1) {
-                    board[y][c] = board[y - 1][c];
-                }
-                board[8][10] = board[7][10];
-            }
-            for (var c = 0; c < gridColumns; c += 1) {
-                board[0][c] = vacant;
-            }
-            score += 10;
-        }
-    }
-    drawBoard();
 }
 var dropStart = Date.now();
-var gameOver = false;
 function drop() {
     var now = Date.now();
     var delta = now - dropStart;
     if (delta > 1000) {
-        piece.moveDown();
+        p.moveDown();
         dropStart = Date.now();
     }
-    if (!gameOver) {
-        requestAnimationFrame(drop);
-    }
+    requestAnimationFrame(drop);
 }
 drop();
+// function randomPiece() {
+//   let randomN = Math.floor(Math.random() * Pieces.length);
+//   return new Piece(Pieces[randomN]);
+// }
+// Piece.prototype.lock = function () {
+//   for (let r = 0; r < this.activeTetromino.length; r += 1) {
+//     for (let c = 0; c < this.activeTetromino.length; c += 1){
+//       if (!this.activeTetromino[r][c]) {
+//         continue;
+//       }
+//       if (this.y + r < 0) {
+//         gameOver = true;
+//         alert('game over!');
+//         break;
+//       }
+//       board[this.y + r][this.x + c] = color;
+//     }
+//   }
+// }
+// function fullRow(){
+//   for (let r = 0; r < gridRows; r += 1){
+//     let isRowFulll = true;
+//     for (let c = 0; c < gridColumns; c += 1){
+//       isRowFulll = isRowFulll&&(board[r][c] != vacant)
+//     }
+//     if (isRowFulll) {
+//       for (let y = r; y > 1; y -= 1){
+//         for (let c = 0; c < gridColumns; c += 1){
+//           board[y][c] = board[y - 1][c];
+//         }
+//         board[8][10] = board[7][10]
+//       }
+//       for (let c = 0; c < gridColumns; c += 1){
+//         board[0][c] = vacant;
+//       }
+//       score += 10;
+//     }
+//   }
+//   drawBoard();
+// }
